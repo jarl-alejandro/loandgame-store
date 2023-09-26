9f6e32bc47c265125ea11623a3594e476dc7b78b
Alejandro <jarlalejor@gmail.com>
2023-09-25 23:05:19 -0500

    agregar talwindcss

--- loandgame-store/app/layout.tsx
+++ loandgame-store/app/layout.tsx
@@ -1,16 +1,32 @@
-import React from "react"
-import layoutStyle from './layout.module.css'
+import React from "react";
+import layoutStyle from './layout.module.css';
+import type { Metadata } from 'next';
+import { Roboto_Condensed } from 'next/font/google';
+
+import './globals.css';
 
 interface Prop {
-    children: React.ReactNode
+  children: React.ReactNode
 }
+
+export const metadata: Metadata = {
+  title: 'Loandgame store',
+  description: 'Crea tiendas para alquilar tus juegos',
+}
+
+const roboto = Roboto_Condensed({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'optional',
+});
 
 export default function RootLayout({ children, }: Prop) {
     return (
-      <html lang="es">
+      <html lang="es" className={roboto.className}>
         <body>
           <main className={layoutStyle.container}>
-            <h1 className={layoutStyle.title}>Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a></h1>
+            <h1 className="text-3xl font-bold">Loandgame store</h1>
             <div className={layoutStyle.container}>
               { children }
             </div>
